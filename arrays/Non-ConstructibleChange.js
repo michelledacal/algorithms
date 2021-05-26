9856fdcd744bb8c07a1c14b1fc3654a3158884f5
Michelle Dacal <michdacal@gmail.com>
2021-05-26 14:57:46 -0400

    easy array algos

--- arrays/Non-ConstructibleChange.js
+++ arrays/Non-ConstructibleChange.js
@@ -1,13 +1,18 @@
-function nonConstructibleChange(coins) {
-	let change = 0;
-	coins.sort((a,b) => a - b);
-	for (let i = 0; i < coins.length; i++) {
-		if (coins[i] > change + 1) return change + 1;
-		change += coins[i];
-	}
-  return change + 1;
-}
-
 // time: O(nlog(n))
 // space: O(1)
 // where n is the number of coins
+
+function nonConstructibleChange(coins) {
+  // sort the coins
+  coins.sort((a,b) => a - b);
+  // set min change to 0
+  let minChange = 0;
+  // loop through coins
+    // if coins[i] <= minChange + 1, add coins[i] to minChange
+    // else break
+  for (let i = 0; i < coins.length; i++) {
+    if (coins[i] <= minChange + 1) minChange += coins[i];
+  }
+  // return minChange + 1
+  return minChange + 1;
+}
--- arrays/TournamentWinner.js
+++ arrays/TournamentWinner.js
@@ -1,22 +1,29 @@
-function tournamentWinner(competitions, results) {
-	let highestScore = 0;
-	let winningTeam = '';
-	let scores = {};
-	
-	for (let i = 0; i < competitions.length; i++) {
-		let [home, away] = competitions[i];
-		let winner = results[i] ? home : away;
-		if (!(winner in scores)) scores[winner] = 0;
-		scores[winner]++;
-		if (scores[winner] > highestScore) {
-			highestScore = scores[winner];
-			winningTeam = winner;
-		}
-	}
-	
-	return winningTeam;
-}
-
 // time: O(n)
 // space: O(k)
 // where n is the number of competitions and k is the number of teams
+
+function tournamentWinner(competitions, results) {
+  // set bestScore to 0 and bestTeam to empty string
+  let bestScore = 0;
+  let bestTeam = '';
+  // set empty hash table with scores of the teams
+  const scores = {};
+  // loop through competitions
+    // grab home and away team and find winner with results[i]
+    // if winning team isn't in table, set score to 0
+    // update winning team score
+    // check if winning team score is greater than bestScore
+      // if it is, set bestScore and bestTeam
+  for (let i = 0; i < competitions.length; i++) {
+    const [homeTeam, awayTeam] = competitions[i];
+    const winningTeam = results[i] ? homeTeam : awayTeam;
+    if (!(winningTeam in scores)) scores[winningTeam] = 0;
+    scores[winningTeam]++;
+    if (scores[winningTeam] > bestScore) {
+      bestScore = scores[winningTeam];
+      bestTeam = winningTeam;
+    }
+  }
+  // return bestTeam
+  return bestTeam;
+}
