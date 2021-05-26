1c3f38125ba0bac52735037f3e8a6da136a15d3a
Michelle Dacal <michdacal@gmail.com>
2021-05-26 14:20:34 -0400

    easy array algos

--- arrays/ValidateSubsequence.js
+++ arrays/ValidateSubsequence.js
@@ -1,11 +1,16 @@
-function isValidSubsequence(array, sequence) {
-	let j = 0;
-		for(let i = 0; i < array.length; i++) {
-			if (sequence[j] === array[i]) j++;
-			if (j === sequence.length) break;
-		}
-	return j === sequence.length;
-}
-
 // time: O(n)
 // space: O(1)
+
+function isValidSubsequence(array, sequence) {
+  // one pointer on sequence
+  let pointer = 0;
+  // loop through array
+    // if array[i] equals sequence[pointer], pointer++
+    // if end of sequence is reached, break
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === sequence[pointer]) pointer++;
+    if (pointer === sequence.length) break;
+  }
+  // return pointer location in sequence
+  return pointer === sequence.length;
+}
