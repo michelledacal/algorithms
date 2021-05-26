8ee0b760378c9c21abad44533695ae749c10223b
Michelle Dacal <michdacal@gmail.com>
2021-05-26 15:24:29 -0400

    medium array algos

--- arrays/SmallestDifference.js
+++ arrays/SmallestDifference.js
@@ -1,23 +1,35 @@
-function smallestDifference(arrayOne, arrayTwo) {
-	arrayOne = arrayOne.sort((a,b) => a-b);
-	arrayTwo = arrayTwo.sort((a,b) => a-b);
-	let i = 0;
-	let j = 0;
-	let absDiff = Math.abs(arrayOne[i] - arrayTwo[j]);
-	let pair = [arrayOne[i], arrayTwo[j]];
-	while (i < arrayOne.length && j < arrayTwo.length) {
-		let diff = Math.abs(arrayOne[i] - arrayTwo[j]);
-		if (diff < absDiff) {
-			absDiff = diff;
-			pair = [arrayOne[i], arrayTwo[j]];
-		} else {
-			if (arrayOne[i] < arrayTwo[j]) i++;
-			else j++;
-		}
-	}
-	return pair;
-}
-
-// time: O(nlog(n) + mlog(m))
+// time: O(mlog(m) + nlog(n))
 // space: O(1)
-// where n is the length of the first array and m is the length of the second array
+// where m is the length of arrayOne and n is the length of arrayTwo
+
+function smallestDifference(arrayOne, arrayTwo) {
+  // sort both of the arrays
+  arrayOne.sort((a,b) => a - b);
+  arrayTwo.sort((a,b) => a - b);
+  // two pointers: one on each array at the beginning
+  let one = 0;
+  let two = 0;
+  // set absDiff to abs(arrayOne[one] - arrayTwo[two])
+  // set diffNums = [arrayOne[one], arrayTwo[two]]
+  let absDiff = Math.abs(arrayOne[one] - arrayTwo[two]);
+  let diffNums = [arrayOne[one], arrayTwo[two]];
+  // while both pointers are less than array lengths
+    // if asbolute difference of pointers is less than absDiff
+      // set absDiff to new diff and diffNums to new nums
+    // else
+      // move pointer that has the lower number
+  while (one < arrayOne.length && two < arrayTwo.length) {
+    const oneNum = arrayOne[one];
+    const twoNum = arrayTwo[two];
+    const diff = Math.abs(oneNum - twoNum);
+    if (diff < absDiff) {
+      absDiff = diff;
+      diffNums = [oneNum, twoNum];
+    } else {
+      if (oneNum < twoNum) one++;
+      else two++;
+    }
+  }
+  // return diffNums
+  return diffNums;
+}
