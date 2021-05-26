8aefc875c9a877bc202ec95df840cf7f08605e0f
Michelle Dacal <michdacal@gmail.com>
2021-05-26 16:12:25 -0400

    medium array algos

--- arrays/MoveElementToEnd.js
+++ arrays/MoveElementToEnd.js
@@ -1,19 +1,23 @@
-function moveElementToEnd(array, toMove) {
-	let left = 0;
-	let right = array.length - 1;
-	while (left < right) {
-		if (array[right] === toMove) right--;
-		if (array[left] === toMove) {
-			let temp = array[right];
-			array[right] = array[left];
-			array[left] = temp;
-		} else {
-			left++;
-		}
-	}
-	return array;
-}
-
 // time: O(n)
 // space: O(1)
-// where n is the length of the input array
+// where n is the length of the array
+
+function moveElementToEnd(array, toMove) {
+  // two pointers: one at the beginning and end of the array
+  let left = 0;
+  let right = array.length - 1;
+  // while left < right
+    // if left !== right, left++
+    // else swap right and left
+    // if right === toMove, right--
+  while (left < right) {
+    if (left !== toMove) left++;
+    else {
+      array[left] = array[right];
+      array[right] = toMove;
+    }
+    if (right === toMove) right--;
+  }
+  // return sorted array
+  return array;
+}
