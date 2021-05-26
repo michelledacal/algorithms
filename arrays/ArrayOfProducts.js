ee4730dbeb33845cd59b0676487836438814ac70
Michelle Dacal <michdacal@gmail.com>
2021-05-26 17:30:41 -0400

    medium array algos

--- arrays/ArrayOfProducts.js
+++ arrays/ArrayOfProducts.js
@@ -1,19 +1,28 @@
-function arrayOfProducts(array) {
-  let products = [];
-	let leftSum = 1;
-	for (let i = 0; i < array.length; i++) {
-		products.push(leftSum)
-		leftSum *= array[i]
-	}
-	
-	let rightSum = 1;
-	for (let i = array.length - 1; i >= 0; i--) {
-		products[i] *= rightSum;
-		rightSum *= array[i];
-	}
-	return products;
-}
-
 // time: O(n)
 // space: O(n)
-// where n is the length of the input array
+// where n is the length of the array
+
+function arrayOfProducts(array) {
+  // set an empty array for products
+  const products = [];
+  // start from the left and set a left product = 1
+  let leftProduct = 1;
+  // loop through the array
+    // push left product to product array
+    // multiply left product by array[i]
+  for (let i = 0; i < array.length; i++) {
+    products.push(leftProduct);
+    leftProduct *= array[i];
+  }
+  // now start at the right and set right product = 1
+  let rightProduct = 1;
+  // loop through the array backward
+    // multiply products[i] by right product
+    // multiply right product by array[i]
+  for (let i = array.length - 1; i >= 0; i--) {
+    products[i] *= rightProduct;
+    rightProduct *= array[i];
+  }
+  // return products
+  return products;
+}
