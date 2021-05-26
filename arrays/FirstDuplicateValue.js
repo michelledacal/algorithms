4e8aca793f62b41dd9150cd6e4ef9507014bddbc
Michelle Dacal <michdacal@gmail.com>
2021-05-26 18:33:41 -0400

    medium array algos

--- arrays/FirstDuplicateValue.js
+++ arrays/FirstDuplicateValue.js
@@ -1,13 +1,19 @@
-function firstDuplicateValue(array) {
-  for (let i = 0; i < array.length; i++) {
-		let absVal = Math.abs(array[i]);
-		let index = absVal - 1;
-		if (array[index] < 0) return absVal;
-		else array[index] *= -1
-	}
-  return -1;
-}
-
 // time: O(n)
 // space: O(1)
-// where n is the length of the input array
+// where n is the length of the array
+
+function firstDuplicateValue(array) {
+  // loop through the array
+    // grab array[i] and set that as index
+    // look at array[index - 1]
+      // if it is negative, return array[i]
+      // else multiply by -1
+  for (let i = 0; i < array.length; i++) {
+    const idx = Math.abs(array[i]);
+    const num = array[idx - 1];
+    if (num < 0) return idx;
+    array[idx - 1] *= -1;
+  }
+  // if no number was returned, return -1
+  return -1;
+}
--- arrays/MergeOverlappingIntervals.js
+++ arrays/MergeOverlappingIntervals.js
@@ -1,22 +1,33 @@
-function mergeOverlappingIntervals(array) {
-	const sortedArr = array.sort((a,b) => a[0] - b[0])
-	let first = sortedArr[0][0];
-	let prev = sortedArr[0][1];
-	let intervals = [];
-	for (let i = 1; i < array.length; i++) {
-		let next = sortedArr[i][0];
-		if (next <= prev) {
-			if (prev < sortedArr[i][1]) prev = sortedArr[i][1];
-		} else {
-			intervals.push([first, prev]);
-			first = next;
-			prev = sortedArr[i][1];
-		}
-	}
-	intervals.push([first, prev]);
-  return intervals;
-}
-
 // time: O(nlog(n))
 // space: O(n)
-// where n is the length of the input array
+// where n is the length of the array
+
+function mergeOverlappingIntervals(array) {
+  // sort array by first num in intervals
+  array.sort((a,b) => a[0] - b[0]);
+  // set empty array to interval array
+  const intervals = [];
+  // set first and last to first set of intervals
+  let first = array[0][0];
+  let last = array[0][1];
+  // loop through array
+    // if array[i][0] <= last
+      // and array[i][1] > last, set last to array[i][1]
+    // else
+      // push [first, last] to intervals
+      // set first and last to current set of interval
+  for (let i = 1; i < array.length; i++) {
+    let next = array[i][0];
+    if (next <= last) {
+      if (array[i][1] > last) last = array[i][1];
+    } else {
+      intervals.push([first, last]);
+      first = next;
+      last = array[i][1];
+    }
+  }
+  // push the last interval
+  intervals.push([first, last]);
+  // return intervals
+  return intervals;
+}
