a4a6d72dd82d0ad999e53959367fee396ebeb1b8
Michelle Dacal <michdacal@gmail.com>
2021-05-26 16:41:51 -0400

    medium array algos

--- arrays/MonotonicArray.js
+++ arrays/MonotonicArray.js
@@ -1,17 +1,25 @@
-function isMonotonic(array) {
-	let type = array[1] - array[0];
-	for(let i = 0; i < array.length - 1; i++) {
-		if (type === 0) {
-			type = array[i+1] - array[i];
-			continue;
-		}
-		let checkType = array[i+1] - array[i];
-		if (type < 0 && checkType > 0 || type > 0 && checkType < 0) {
-			return false;
-		} 
-	}
-	return true;
-}
 // time: O(n)
 // space: O(1)
 // where n is the length of the array
+
+function isMonotonic(array) {
+  // subtract first two numbers in the array to find the direction
+  let direction = array[1] - array[0];
+  // loop through the array
+    // if direction is 0, find diff of next two numbers of array
+    // else
+      // find diff of next two numbers
+      // if direction > 0 && diff < 0, return false
+      // if direction < 0 && diff > 0, return false
+  for (let i = 1; i < array.length; i++) {
+    const diff = array[i + 1] - array[i];
+    if (!direction) {
+      direction = diff;
+      continue;
+    }
+    if (direction > 0 && diff < 0) return false;
+    if (direction < 0 && diff > 0) return false;
+  }
+  // return true if it makes it to the end
+  return true;
+}
--- arrays/ThreeNumberSum.js
+++ arrays/ThreeNumberSum.js
@@ -1,23 +1,38 @@
-function threeNumberSum(array, targetSum) {
-	let triplets = [];
-	array.sort((a,b) => a-b)
-	for (let i = 0; i < array.length - 2; i++) {
-		let left = i + 1;
-		let right = array.length - 1;
-		while (left < right) {
-			const sum = array[i] + array[left] + array[right];
-			if (sum > targetSum) right--;
-			if (sum < targetSum) left++;
-			if (sum === targetSum) {
-				triplets.push([array[i], array[left], array[right]])
-				left++;
-				right--;
-			}
-		}
-	}
-	return triplets;
-}
-
 // time: O(n^2)
 // space: O(n)
-// where n is the length of the input array
+// where n is the length of the array
+
+function threeNumberSum(array, targetSum) {
+  // sort the array
+  array.sort((a,b) => a - b);
+  // set empty array for triplets
+  const triplets = [];
+  // loop through the array
+    // set two pointers: one at [i + 1] and at the end
+    // while left < right
+      // sum all the numbers
+      // if sum equals targetSum
+        // push nums to targetSum
+        // move both pointers
+      // if sum < targetSum, left++
+      // if sum > targetSum, right--
+  for (let i = 0; i < array.length; i++) {
+    const num = array[i];
+    let left = i + 1;
+    let right = array.length - 1;
+    while (left < right) {
+      const leftNum = array[left];
+      const rightNum = array[right];
+      const sum = num + leftNum + rightNum;
+      if (sum === targetSum) {
+        triplets.push([num, leftNum, rightNum]);
+        left++;
+        right--;
+      }
+      if (sum < targetSum) left++;
+      if (sum > targetSum) right--;
+    }
+  }
+  // return triplets
+  return triplets;
+}
