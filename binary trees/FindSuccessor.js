0ca01d33d9a65d761e906e0cb40579bf8a564d42
Michelle Dacal <michdacal@gmail.com>
2021-05-27 10:21:49 -0400

    medium binary tree algos

--- binary trees/FindSuccessor.js
+++ binary trees/FindSuccessor.js
@@ -1,29 +1,28 @@
-class BinaryTree {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-    this.parent = null;
-  }
-}
-
-function findSuccessor(tree, node) {
-	if (node.right) return goLeft(node.right);
-  
-	return goUpParents(node);
-	
-	function goLeft(node) {
-		if (node.left) return goLeft(node.left);
-		return node;
-	}
-	
-	function goUpParents(node) {
-		if (node.parent && node.parent.right === node) return goUpParents(node.parent);
-		return node.parent;
-	}
-	
-}
-
 // time: O(h)
 // space: O(1)
 // where h is the height of the tree
+
+function findSuccessor(tree, node) {
+  // if node.right exists, go to node.right and then go as far left
+  if (node.right) return goLeft(node.right);
+  // else
+    // go up to parent and see if coming from left
+      // if yes, return parent
+      // if no, go up to parent and repeat
+  return goUpToParent(node);
+
+  function goLeft(node) {
+    while (node.left) {
+      node = node.left;
+    }
+    return node;
+  }
+
+  function goUpToParent(node) {
+    while (node.parent && node.parent.right === node) {
+      node = node.parent;
+    }
+    return node.parent;
+  }
+
+}
